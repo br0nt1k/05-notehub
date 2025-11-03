74cb9f2d9f7f8b5bda32211299e19cbd9e8c9a86
Oleh Oprysk <opryskoleh2006@gmail.com>
2025-11-03 18:01:55 +0200

    finished hw

---
+++ src/components/App/App.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import NoteList from "../NoteList/NoteList";
+import css from "./App.module.css";
+import Pagination from "../Pagination/Pagination";
+import SearchBox from "../SearchBox/SearchBox";
+import NoteModal from "../NoteModal/NoteModal";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { fetchNotes } from "../../services/noteService";
+import { useDebounce } from "use-debounce";
+
+export default function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [debouncedSearchQuery] = useDebounce(searchQuery, 300);
+
+  const { data } = useQuery({
+    queryKey: ["notes", debouncedSearchQuery, currentPage],
+    queryFn: () => fetchNotes(debouncedSearchQuery || "", currentPage),
+    placeholderData: keepPreviousData,
+  });
+
+  const handleSearch = (query: string) => {
+    setSearchQuery(query);
+    setCurrentPage(1);
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const notes = data?.notes;
+
+  return (
+    <div className={css.app}>
+      <header className={css.toolbar}>
+        <SearchBox searchQuery={searchQuery} onChange={handleSearch} />
+        {data && data.totalPages > 1 && (
+          <Pagination
+            currentPage={currentPage}
+            onPageChange={setCurrentPage}
+            totalPages={data.totalPages}
+          />
+        )}
+        <button className={css.button} onClick={openModal}>
+          Create note +
+        </button>
+      </header>
+      {notes && notes.length > 0 && <NoteList notes={notes} />}
+      {isModalOpen && <NoteModal onClose={closeModal} />}
+    </div>
+  );
+}
